a072f6a66358264484bdfad6f7e85f4df143d3a5
Pulkit Kathuria <kevincobain2000@gmail.com>
2024-01-28 15:56:31 +0900

    test echo (#5)
    * test echo
    
    * destroy tests basic
    
    * pr handler tests basic
    
    * pr handler fix

---
+++ app/pkg/destroy_handler_test.go
@@ -0,0 +1,50 @@
+package pkg
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDestroyErrors(t *testing.T) {
+
+	BeforeEach()
+	defer AfterEach()
+	e := echo.New()
+	e.POST("/destroy", func(c echo.Context) error {
+		return NewDestroyHandler().Post(c)
+	})
+
+	server := httptest.NewServer(e)
+	defer server.Close()
+
+	type TestCase struct {
+		Name   string
+		Body   []byte
+		Status int
+	}
+	testCases := []TestCase{
+		{
+			Name:   "422",
+			Body:   []byte(`{"repo":"repo","branch":"branch","type":"type"}`),
+			Status: http.StatusUnprocessableEntity,
+		},
+		{
+			Name:   "200",
+			Body:   []byte(`{"org":"org", "repo":"repo","branch":"branch","type":"type"}`),
+			Status: http.StatusOK,
+		},
+	}
+
+	for _, tc := range testCases {
+		url := server.URL + "/destroy"
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(tc.Body))
+
+		assert.NoError(t, err)
+		assert.Equal(t, tc.Status, resp.StatusCode)
+	}
+}
